0256dc04f9f2f94a4dc1ee8a674edb38f3e71d44
Sean Fitzgerald <sean@fitzgeralds.me>
2015-09-13 16:33:48 -0700

    Removing conceptnet code, creating packaging script.

---
+++ setup.py
@@ -0,0 +1,24 @@
+__author__ = 'seanfitz'
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "adapt",
+    version = "0.0.1",
+    author = "Sean Fitzgerald",
+    author_email = "sean@fitzgeralds.me",
+    description = ("A text-to-intent parsing framework."),
+    license = ("LGPL-3"),
+    keywords = "natural language processing",
+    url = "https://github.com/MycroftAI/adapt",
+    packages = ["adapt"],
+    long_description=read('README.md'),
+    dependency_links = [
+        "pyee"
+    ]
+)
