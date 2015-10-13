a4515eefff45d5d238116ef158089073771bf5da
Sean Fitzgerald <sean@fitzgeralds.me>
2015-10-12 23:49:16 -0700

    Learning new things about python packaging every day.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license = ("LGPL-3"),
     keywords = "natural language processing",
     url = "https://github.com/MycroftAI/adapt",
-    packages = ["adapt"],
+    packages = ["adapt", "adapt.tools", "adapt.tools.text"],
     long_description=read('README.md'),
     dependency_links = [
         "pyee"
