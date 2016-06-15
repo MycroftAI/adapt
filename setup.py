0ed60ad171b037f535ba7b855b185de501aa5915
aatchison <arron.atchison@gmail.com>
2016-06-15 10:16:31 -0500

    Changed hard-coded version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "adapt-parser",
-    version = "0.2.3",
+    version = "0.2.4",
     author = "Sean Fitzgerald",
     author_email = "sean@fitzgeralds.me",
     description = ("A text-to-intent parsing framework."),
