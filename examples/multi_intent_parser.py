e764e9949e65d6182c52b77c5c2d7ee6bd3369c5
Sean Fitzgerald <sean@fitzgeralds.me>
2020-09-17 20:04:23 -0700

    Issue #109: unused imports/variable declarations
    Fixes issue #109

--- examples/multi_intent_parser.py
+++ examples/multi_intent_parser.py
@@ -10,17 +10,8 @@ PYTHONPATH=. python examples/multi_intent_parser.py "play some music by the clas
 
 import json
 import sys
-from adapt.entity_tagger import EntityTagger
-from adapt.tools.text.tokenizer import EnglishTokenizer
-from adapt.tools.text.trie import Trie
 from adapt.intent import IntentBuilder
-from adapt.parser import Parser
 from adapt.engine import IntentDeterminationEngine
-
-tokenizer = EnglishTokenizer()
-trie = Trie()
-tagger = EntityTagger(trie, tokenizer)
-parser = Parser(tokenizer, tagger)
 
 engine = IntentDeterminationEngine()
 
