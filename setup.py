2cbcdf6516f1b5e379bb8335029f8b8ddbb9dc35
Åke Forslund <ake.forslund@gmail.com>
2020-11-02 12:11:57 +0100

    Add python 3.9 as a supported version
    Also removes python 3.4 as no tests are run on that version.

--- setup.py
+++ setup.py
@@ -38,11 +38,11 @@ setup(
         'Topic :: Text Processing :: Linguistic',
         'License :: OSI Approved :: Apache Software License',
 
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     install_requires=[
